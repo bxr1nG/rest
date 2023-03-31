e61441be9b791788b3f497af432a81c05ab8da1f
bxr1nG <vochshukdaniil@gmail.com>
2023-03-31 21:02:41 +0300

    feat(server): add routes for database tables and table columns

--- packages/server/src/routes/api/routes.ts
+++ packages/server/src/routes/api/routes.ts
@@ -2,12 +2,23 @@ import { Router } from "express";
 
 import type Params from "~/types/Params";
 import ErrorHandler from "~/middlewares/ErrorHandler";
+import Base from "~/utils/strategies/ORM/Base";
 
 import controllers from "./controllers";
 
 const router = Router();
 
 router
+    .get("/tables", (_req, res, next) => {
+        Base.getTables()
+            .then((result) => res.status(200).json(result))
+            .catch((error) => next(error));
+    })
+    .get("/columns/:table", (req, res, next) => {
+        Base.getTableColumns(req.params.table)
+            .then((result) => res.status(200).json(result))
+            .catch((error) => next(error));
+    })
     .get("/table/:table", (req, res, next) => {
         controllers
             .getAll(req.params.table, req.query as Params)
